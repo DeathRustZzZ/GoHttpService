5ecef9dac0d7402f4d5b433ee75db87a55c5d48c
DeathCrushZzZ <Rustforthefuture@yandex.ru>
2026-08-11 22:17:33 +0300

    feat(server): add simple http handler and switch port to 8080

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,8 +4,16 @@ import (
 	"net/http"
 )
 
+type MyHandler struct{}
+
+func (h MyHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	data := []byte("Hello")
+	res.Write(data)
+}
+
 func main() {
-	err := http.ListenAndServe(`:8000`, nil)
+	var h MyHandler
+	err := http.ListenAndServe(`:8080`, h)
 	if err != nil {
 		panic(err)
 	}
